fe3f14c4c3c05050436a65412971b36c98398827
Jorge Solano <jorgesolano92@gmail.com>
2023-02-01 05:04:21 -0400

    cambio de identificador en config.xml

--- src/app/services/consultas.service.ts
+++ src/app/services/consultas.service.ts
@@ -20,7 +20,7 @@ export class ConsultasService {
 
   url = environment.apiUrl;
 
-  public actualV = '1.0.42';
+  public actualV = '1.0.43';
 
   constructor(private http: HttpClient, private file: _File, private platform: Platform, private keyboard: Keyboard) {
   }
