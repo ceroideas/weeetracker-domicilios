e950ac6ecb674bd6c46a5a934c3d1b435e30eccb
Jorge Solano <jorgesolano92@gmail.com>
2023-03-01 05:03:44 -0400

    cambios en validaciones

--- src/app/services/consultas.service.ts
+++ src/app/services/consultas.service.ts
@@ -20,7 +20,7 @@ export class ConsultasService {
 
   url = environment.apiUrl;
 
-  public actualV = '1.0.48';
+  public actualV = '1.0.49';
 
   constructor(private http: HttpClient, private file: _File, private platform: Platform, private keyboard: Keyboard) {
   }
